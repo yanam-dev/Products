2e16db5e50658292701c22f92553b22ae3bb14f9
yanam-dev <yamusjko@gmail.com>
2020-12-11 02:58:33 +0100

    added section with positioning + slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.slider__items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrow: false
+      });
 });
